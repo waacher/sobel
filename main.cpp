470280e5cbd223086eb4cc8268264e8d5d07f136
waacher <alshan@calpoly.edu>
2019-11-07 13:38:53 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
@@ -5,129 +6,193 @@
 #include <cmath>
 #include <iostream>
 #include <stdio.h>
+#include <time.h>
+
 
 using namespace cv;
 using namespace std;
 
-Mat to442_grayscale(Mat frame)
+Mat *top_grey;
+Mat *top_sobel;
+
+void * to442_grayscale(Mat * frame)
 {
-	CV_Assert(frame.type() == CV_8UC3);
+    Mat *ptr = frame;
+    //CV_Assert(frame.type() == CV_8UC3);
 
-	int channels = frame.channels();
-	int nRows = frame.rows;
-	int nCols = frame.cols * channels;
+    int channels = frame->channels();
+    int nRows = frame->rows;
+    int nCols = frame->cols * channels;
 
-	for(int i = 0; i < nRows; i++)
-	{
-		uchar *row = frame.ptr(i);
-		for(int j = 0; j < nCols; j+=3)
-		{
-			uchar pixelB = (int)row[j]*.0722;
-			uchar pixelG = (int)row[j+1]*.7152;
-			uchar pixelR = (int)row[j+2] *.2126;
-			int  grey = pixelB + pixelG + pixelR;
-	
-			if(grey > 255)
-			{
-				grey = 255;
-			}
+    int i, j;
+    #pragma omp simd collapse(2)
+	for( i = 1; i < nRows-1; i++)
+    {
+        //uchar *row = frame->ptr(i);
+        for( j = 3; j < nCols-3; j+=3)
+        {
+            uchar pixelB = frame->ptr(i)[j] * 0.0722;
+            uchar pixelG = frame->ptr(i)[j+1]*.7152;
+            uchar pixelR = frame->ptr(i)[j+2] *.2126;
+            int  grey = pixelB + pixelG + pixelR;
 
-			row[j] = grey;
-			row[j+1] = grey;
-			row[j+2] = grey;
-		}
-	}
-	return frame;
-}	
+            /* No if statements in pragma
+            if(grey > 255)
+            {
+                grey = 255;
+            } */
 
-Mat to442_sobel(Mat frame)
+            frame->ptr(i)[j] = grey;
+            frame->ptr(i)[j+1] = grey;
+            frame->ptr(i)[j+2] = grey;
+        }
+    }
+    return (void *)ptr;
+}
+
+void * to442_sobel(Mat *frame)
 {
-    CV_Assert(frame.type() == CV_8UC3);
-	Mat dst = frame.clone();	//destination mat that will have black border
-	CV_Assert(dst.type() == CV_8UC3);
-	int top, bottom, left, right;
-	top = 1 ; 	//amount we are adding to our frame
-	bottom = top;
-	left = 1 ;
-	right = left;
-	copyMakeBorder(frame, dst, top, bottom, left, right, 1, 0);
-	//copy make border, frame is source, dst is destination
-	//top, botoom,left and right = 1
-	//this will add one row to top and bottom and
-	//one column to left and right of our frame
-	Mat new_dest = dst.clone();
-	int channels = dst.channels();
-	int  nRows = dst.rows;
-	int nCols = dst.cols * channels;
+    Mat cpy = frame->clone();
+    Mat *ptr = frame;
+    CV_Assert(frame->type() == CV_8UC3);
+    int channels = frame->channels();
+    int nRows = frame->rows;
+    int nCols = frame -> cols * channels;
 
-	for(int i = 1; i < nRows-1; i++)
-	{
-		uchar *row_dest = new_dest.ptr(i);
-		for(int j = 3; j < nCols-3; j+=3)
-		{
-			
-			uchar pixelUL = dst.ptr(i-1)[j-3];
-			uchar pixelUC = dst.ptr(i-1)[j];
-			uchar pixelUR = dst.ptr(i-1)[j+3];
-			uchar pixelCL = dst.ptr(i)[j-3];
-			
-			uchar pixelCR = dst.ptr(i)[j+3];
-			uchar pixelBL = dst.ptr(i+1)[j-3];
-			uchar pixelBC = dst.ptr(i+1)[j];
-			uchar pixelBR = dst.ptr(i+1)[j+3];
-			
-			int gxUL = pixelUL * -1;
-			int gxUR = pixelUR;
-			int gxCL = pixelCL * -2;
-			int gxCR = pixelCR * 2;
-			int gxBL = pixelBL * -1;
-			int gxBR = pixelBR;
-			
-			int gyUL = pixelUL;
-			int gyUC = pixelUC * 2;
-			int gyUR = pixelUR;
-			int gyBL = pixelBL * -1;
-			int gyBC = pixelBC * -2;
-			int gyBR = pixelBR * -1;
-			
-			int gx_scal = abs(gxUL + gxUR + gxCL + gxCR + gxBL + gxBR);
-			int gy_scal = abs(gyUL + gyUC + gyUR + gyBL + gyBC + gyBR);
-			
-			int g = gx_scal + gy_scal;
-			if(g > 255)
-			{
-				g = 255;
-			}
-			row_dest[j] = g;
-			row_dest[j+1] =g;
-			row_dest[j+2] = g;	
-		}
-	}
-	return new_dest;
+    int i, j;
+    #pragma omp simd  collapse(2)
+    for( i = 1; i < nRows-1; i++)
+    {
+        for( j = 3; j < nCols-3; j+=3)
+        {
+	    uchar pixelUL = cpy.ptr(i-1)[j-3];
+	    uchar pixelUC = cpy.ptr(i-1)[j];
+     	    uchar pixelUR = cpy.ptr(i-1)[j+3];
+	    uchar pixelCL = cpy.ptr(i)[j-3];
+            uchar pixelCR = cpy.ptr(i)[j+3];
+	    uchar pixelBL = cpy.ptr(i+1)[j-3];
+    	    uchar pixelBC = cpy.ptr(i+1)[j];
+	    uchar pixelBR = cpy.ptr(i+1)[j+3];
+
+	    int gxUL = pixelUL * -1;
+       	    int gxUR = pixelUR;
+	    int gxCL = pixelCL * -2;
+	    int gxCR = pixelCR * 2;
+	    int gxBL = pixelBL * -1;
+            int gxBR = pixelBR;
+	    int gyUL = pixelUL;
+	    int gyUC = pixelUC * 2;
+	    int gyUR = pixelUR;
+	    int gyBL = pixelBL * -1;
+	    int gyBC = pixelBC * -2;
+	    int gyBR = pixelBR * -1;
+
+	    int gx_scal = abs(gxUL + gxUR + gxCL + gxCR + gxBL + gxBR);
+	    int gy_scal = abs(gyUL + gyUC + gyUR + gyBL + gyBC + gyBR);
+
+	    int g = gx_scal + gy_scal;
+	    if(g > 255)
+            {
+	         g = 255;
+	    }
+	    frame->ptr(i)[j] = g;
+	    frame->ptr(i)[j+1] =g;
+	    frame->ptr(i)[j+2] = g;
+        }
+    }
+    return (void *) ptr;
+}
+
+Mat combine(Mat original, Mat frame_top, Mat frame_bot)
+{
+    int channels = original.channels();
+    int nRows = original.rows;
+    int nCols = original.cols * channels;
+
+    for(int i = 0; i < nRows/2+1; i++)
+    {
+	uchar *row = original.ptr(i);
+        for(int j = 0; j < nCols; j++)
+        {
+            row[j] = frame_top.ptr(i)[j];
+        }
+    }
+    int x = 2;
+    int y = 0;
+    for(int i = nRows/2-1; i < nRows; i++)
+    {
+        uchar *row = original.ptr(i);
+        for(int j = 0; j < nCols; j++)
+        {
+            row[j] = frame_bot.ptr(x)[y++];
+        }
+        y = 0;
+        x++;
+    }
+    return original;
+}
+
+void * thread_func(void * arg)
+{
+    Mat *frame = (Mat *) arg;
+    top_grey = (Mat *)to442_grayscale(frame);
+    top_sobel = (Mat *)to442_sobel(top_grey);
+    pthread_exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-	VideoCapture cap(argv[1]);
-	Mat frame;
-	
-	while(true)
+    VideoCapture cap("video_forcv.mp4");
+    Mat frame;
+    clock_t t;
+    Mat *frame_pointer_bot_grey;
+    frame_pointer_bot_grey = &frame;
+    Mat dst;
+    Mat *frame_pointer_bot_sobel;
+    int top, bottom, left, right;
+    top = 1 ; 	//amount we add to our frame
+    bottom = top;
+    left = 1 ;
+    right = left;
+    pthread_t t1;
+    int num_frames = 0;
+
+    t = clock();
+    while(true)
+    {
+	cap.read(frame);
+
+        CV_Assert(frame.type() == CV_8UC3);
+        num_frames++;
+	if(frame.empty())
 	{
-		cap.read(frame);
-		if(frame.empty())
-		{
-			break;
-		}
-		CV_Assert(frame.type() == CV_8UC3);
-		
-		Mat frame2 = to442_grayscale(frame);
-		Mat frame3 = frame;
-		frame3 = to442_sobel(frame3);
-		
-        imshow("hello_andy", frame3);
-		waitKey(100);
+	     break;
 	}
-	cap.release();
-	destroyAllWindows();
-	return 0;
+        // copy make border, frame is source, dst is destination
+        // top, botoom,left and right = 1
+        // this will add one row to top and bottom and
+        // one column to left and right of our frame
+	copyMakeBorder(frame, dst, top, bottom, left, right, 0, 0);
+        Mat cpy = dst.clone();
+        Mat croppedFrameTop = dst(Rect(0, 0, dst.cols, dst.rows/2+1));
+        Mat croppedFrameBot = dst(Rect(0, dst.rows/2-1 ,dst.cols, dst.rows/2+1));
+
+        // Grayscale and Sobel on bottom
+        pthread_create(&t1, NULL, thread_func, &croppedFrameTop);
+
+        // Grayscale and Sobel on top
+        frame_pointer_bot_grey =(Mat*)to442_grayscale(&croppedFrameBot);
+	frame_pointer_bot_sobel  = (Mat*)to442_sobel(frame_pointer_bot_grey);
+
+        pthread_join(t1, NULL);
+
+        Mat combined_frame = combine(cpy, *top_sobel, *frame_pointer_bot_sobel);
+        imshow("top_half", combined_frame);
+        waitKey(50);
+    }
+    t = clock() -t ;
+    printf("frames per second: %f\n",num_frames/( ((float)t) / CLOCKS_PER_SEC));
+    cap.release();
+    destroyAllWindows();
+    return 0;
 }
